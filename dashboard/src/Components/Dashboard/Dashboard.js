319908344cfc9c4017d2d0e66ff68947fab44a0c
remoo1901 <remoo1901@hotmail.com>
2020-04-21 20:11:16 -0400

    Still working on dashboard.js

--- dashboard/src/Components/Dashboard/Dashboard.js
+++ dashboard/src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./Dashboard.scss";
 import { server } from "./../../axios";
+import { useHistory } from "react-router-dom";
 
 function Dashboard() {
-  const [Cars, setCars] = useState([]);
+  const [cars, setCars] = useState([]);
+  const history = useHistory();
   useEffect(() => {
     server
       .get("/cars")
@@ -14,6 +16,36 @@ function Dashboard() {
   return (
     <div>
       <h1>Dashoard</h1>
+      <button
+        onClick={() => {
+          localStorage.clear();
+          history.push("/");
+        }}
+      >
+        Sign Out
+      </button>
+      <div className="information">
+        <p>Number of Cars in Inventory:{cars.length}</p>
+        <p>
+          MSRP of Inventory: ${" "}
+          {cars
+            .reduce((accumulator, item) => {
+              return (accumulator = accumulator + Number(item.price));
+            }, 0)
+            .toLocaleString("USD")}
+        </p>
+      </div>
+      <div className="currentInventory">
+        {cars.map((car) => {
+          return (
+            <p>
+              {car.year},{car.make}, {car.model}, {car.miles}, {car.color},{" "}
+              {car.vin} <button>Delete</button>
+              <button>Edit</button>
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 }
